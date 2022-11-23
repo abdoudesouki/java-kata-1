d038fdb52ea5c519451b0cb16de22a92cee1cec5
Abdelmoneim Desouki <31845116+abdoudesouki@users.noreply.github.com>
2022-11-23 09:50:08 +0100

    BooksController

--- src/main/java/org/echocat/kata/java/part1/BooksController.java
+++ src/main/java/org/echocat/kata/java/part1/BooksController.java
@@ -1,13 +1,15 @@
 package org.echocat.kata.java.part1;
 
 import org.springframework.http.MediaType;
+
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 public class BooksController {
 
--- src/test/java/org/echocat/kata/java/part1/MainAppUnitTest.java
+++ src/test/java/org/echocat/kata/java/part1/MainAppUnitTest.java
@@ -5,11 +5,51 @@ import org.junit.Test;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
+
 public class MainAppUnitTest {
 
-    @Test
-    public void testGetHelloWorldText() {
-        assertThat(MainApp.getHelloWorldText(), is("Hello world!"));
-    }
+//    @Test
+//    public void testGetHelloWorldText() {
+//        assertThat(MainApp.getHelloWorldText(), is("Hello world!"));
+//    }
+	
+	public static String getBookData(String isbn) throws IOException{
+
+		HttpURLConnection connection = (HttpURLConnection) new URL("http://localhost:8080/books/" + isbn).openConnection();
+		
+		connection.setRequestMethod("GET");
+
+		int responseCode = connection.getResponseCode();
+		if(responseCode == 200){
+			String response = "";
+			Scanner scanner = new Scanner(connection.getInputStream());
+			while(scanner.hasNextLine()){
+				response += scanner.nextLine();
+				response += "\n";
+			}
+			scanner.close();
+
+			return response;
+		}
+		
+		// an error happened
+		return null;
+	}
+	
+	@Test
+	public void testBookApi() {
+		try {
+			System.out.println(getBookData("123"));
+			assertThat(getBookData("123"),is(null));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
 
 }
